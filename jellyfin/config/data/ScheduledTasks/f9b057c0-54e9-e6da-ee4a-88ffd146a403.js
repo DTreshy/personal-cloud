91428c952d5be0de0a0689c7733a088c89ad7dce
Damian Szczukowski <damian.szczukowski@comarch.com>
2023-02-11 18:04:37 +0100

    xd

---
+++ jellyfin/config/data/ScheduledTasks/f9b057c0-54e9-e6da-ee4a-88ffd146a403.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2023-02-11T17:04:19.1204769Z","EndTimeUtc":"2023-02-11T17:04:20.0595172Z","Status":"Completed","Name":"Update Plugins","Key":"PluginUpdates","Id":"f9b057c054e9e6daee4a88ffd146a403"}
